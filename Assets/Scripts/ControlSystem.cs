8996eacfc2fe8bc7a6dc03563645042e09dcf349
boilleaf <131576538+boilleaf@users.noreply.github.com>
2023-04-23 15:06:52 +0800

    Update

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class ControlSystem : MonoBehaviour
 {
+	[Header("移動速度"),Range(0.5f,9.9f)]
+	public float movespeed = 3.5f;
 
+	[Header("2D 鋼體")]
+	public Rigidbody2D rig;
+
+	private void Awake()
+	{
+		print("hellowworld");
+		print("x+x");
+	}
+	private void Start()
+	{
+		print("<color=red>開始");
+
+	}
+	private void Update()
+	{
+		print("更新");
+	}
 }
